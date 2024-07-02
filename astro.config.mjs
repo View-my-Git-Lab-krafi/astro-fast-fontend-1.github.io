f61f6544db4e83d2420dd4952fa24b62cdbcf0aa
kazi ar rafi <121858831+View-my-Git-Lab-krafi@users.noreply.github.com>
2024-07-02 16:04:38 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 
 import { defineConfig } from 'astro/config';
 
-let BASE_URL = LOCALHOST_URL;
-// When you're building your site in local or in CI, you could just set your URL manually
-if (isBuild) {
-BASE_URL = LIVE_URL;
-}
+
 export default defineConfig({
   site: 'https://View-my-Git-Lab-krafi.gitlab.io',
   base: '/astro-fast-fontend-1.github.io',
